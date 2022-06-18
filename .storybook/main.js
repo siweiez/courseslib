9bf4ca87f8d52f1342b8bd3f47b5482d1c0e22cc
siweiez <102887631+siweiez@users.noreply.github.com>
2022-06-17 21:03:29 -0700

    theme change

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,5 +13,9 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
-  }
+  },
+  // https://github.com/storybookjs/storybook/issues/10231
+  "features": {
+    "emotionAlias": false,
+  },
 }
