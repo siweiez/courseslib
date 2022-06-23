ecb0a411abf454b5f144a461782f4774034e9d98
siweiez <102887631+siweiez@users.noreply.github.com>
2022-06-23 00:37:18 -0700

    login with react hook form

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import styled from "@emotion/styled";
+import { useForm } from "react-hook-form";
 import { CenteredTile } from "@/components/Tile";
-import { Input } from "@/components/Input";
+import { Input, Feedback } from "@/components/Input";
 import { Button } from "@/components/Button";
 import { StyledLink } from "@/components/StyledLink";
 
@@ -10,29 +11,67 @@ const StyledInput = styled(Input)`
   margin-bottom: 1rem;
 `;
 
+export type LoginForm = {
+  identifier: string;
+  password: string;
+};
+
 const Login: NextPage = () => {
-  const onSubmit = () => {};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>();
+
+  const onSubmit = (data: LoginForm) => {
+    console.log(data);
+  };
+
   return (
-    <>
-      <br />
-      <br />
-      <form onSubmit={onSubmit}>
-        <CenteredTile header="Login">
-          <StyledInput label="Identifier" placeholder="username or email" />
-          <StyledInput
-            label="Password"
-            type="password"
-            placeholder="password"
-          />
-          <Button type="submit">Sign In</Button>
-          <h3>
-            <Link href="/registration" passHref>
-              <StyledLink underline>Create account</StyledLink>
-            </Link>
-          </h3>
-        </CenteredTile>
-      </form>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <CenteredTile header="Login">
+        <StyledInput
+          label="Identifier"
+          placeholder="username or email"
+          feedback={
+            errors.identifier ? (
+              <Feedback>{errors.identifier?.message}</Feedback>
+            ) : (
+              <>&nbsp;</>
+            )
+          }
+          height={8}
+          {...register("identifier", {
+            required: "Required field!",
+            minLength: { value: 6, message: "Min length 6!" },
+          })}
+        />
+        <StyledInput
+          label="Password"
+          type="password"
+          placeholder="password"
+          role="textbox"
+          feedback={
+            errors.password ? (
+              <Feedback>{errors.password?.message}</Feedback>
+            ) : (
+              <>&nbsp;</>
+            )
+          }
+          height={8}
+          {...register("password", {
+            required: "Required field!",
+            minLength: { value: 8, message: "Min length 8!" },
+          })}
+        />
+        <Button type="submit">Sign In</Button>
+        <h3>
+          <Link href="/registration" passHref>
+            <StyledLink underline>Create account</StyledLink>
+          </Link>
+        </h3>
+      </CenteredTile>
+    </form>
   );
 };
 
