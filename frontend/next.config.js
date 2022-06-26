12df6e948a426d738714608d83b7aaefb0270f83
siweiez <102887631+siweiez@users.noreply.github.com>
2022-06-25 20:22:29 -0700

    connect to api using heroku

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost:1337", "localhost"],
+    domains: [
+      "localhost:1337",
+      "localhost",
+      "courseslib-backend.herokuapp.com",
+    ],
   },
 };
 
